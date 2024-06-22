93d2d092d92f726aacf11c7e07426793a6334792
R. Quast <octoflar@icloud.com>
2024-06-22 02:19:57 +0200

    Update solver_mod.F90

--- src/main/fortran/solver_mod.F90
+++ src/main/fortran/solver_mod.F90
@@ -17,91 +17,19 @@ module solver_mod
     
   interface cg
     module procedure cg__dp
-    module procedure cg_diagonal_matrix__dp
     module procedure cg_symmetric_matrix__dp
   end interface
 
   interface cg_ad
-    module procedure cg_diagonal_matrix_ad__dp
     module procedure cg_symmetric_matrix_ad__dp
   end interface
 
   interface cg_tl
-    module procedure cg_diagonal_matrix_tl__dp
     module procedure cg_symmetric_matrix_tl__dp
   end interface
 
 contains
       
-  subroutine cg_diagonal_matrix__dp( n, a, b, x, accuracy_goal, max_iteration )
-    integer, intent(in) :: n
-    real(kind=dp), intent(in) :: a(n)
-    real(kind=dp), intent(in) :: b(n)
-    real(kind=dp), intent(out) :: x(n)
-    real(kind=dp), intent(in) :: accuracy_goal
-    integer, intent(in) :: max_iteration
-        
-    call cg__dp( n, a_matmul, b, x, m_inv_matmul, accuracy_goal, max_iteration )
-        
-  contains
-        
-    pure function a_matmul( n, x ) result (y)
-      use base_mod, only: dp
-        
-      integer, intent(in) :: n
-      real(kind=dp), intent(in) :: x(n)
-      real(kind=dp) :: y(n)
-        
-      y = x * a
-    end function a_matmul
-      
-    !> @brief The inverse Jacobi preconditioning matrix.
-    pure function m_inv_matmul( n, x ) result (y)
-      use base_mod, only: dp
-      
-      integer, intent(in) :: n
-      real(kind=dp), intent(in) :: x(n)
-      real(kind=dp) :: y(n)
-      
-      y = x / a
-    end function m_inv_matmul
-  end subroutine cg_diagonal_matrix__dp
-
-  subroutine cg_diagonal_matrix_ad__dp( n, a, a_ad, b, b_ad, x, x_ad, accuracy_goal, max_iteration )
-    integer, intent(in) :: n
-    real(kind=dp), intent(in) :: a(n)
-    real(kind=dp), intent(inout) :: a_ad(n)
-    real(kind=dp), intent(in) :: b(n)
-    real(kind=dp), intent(inout) :: b_ad(n)
-    real(kind=dp), intent(inout) :: x(n)
-    real(kind=dp), intent(inout) :: x_ad(n)
-    real(kind=dp), intent(in) :: accuracy_goal
-    integer, intent(in) :: max_iteration
-    
-    real(kind=dp) :: h(n)
-
-    call cg( n, a, b, x, accuracy_goal, max_iteration )
-    call cg( n, a, x_ad, h, accuracy_goal, max_iteration )
-    b_ad = b_ad + h
-    a_ad = a_ad - h * x
-    x_ad = 0.0_dp
-  end subroutine cg_diagonal_matrix_ad__dp
-
-  subroutine cg_diagonal_matrix_tl__dp( n, a, a_tl, b, b_tl, x, x_tl, accuracy_goal, max_iteration )
-    integer, intent(in) :: n
-    real(kind=dp), intent(in) :: a(n)
-    real(kind=dp), intent(in) :: a_tl(n)
-    real(kind=dp), intent(in) :: b(n)
-    real(kind=dp), intent(in) :: b_tl(n)
-    real(kind=dp), intent(inout) :: x(n)
-    real(kind=dp), intent(inout) :: x_tl(n)
-    real(kind=dp), intent(in) :: accuracy_goal
-    integer, intent(in) :: max_iteration
-
-    call cg( n, a, b, x, accuracy_goal, max_iteration )
-    call cg( n, a, b_tl - a_tl * x, x_tl, accuracy_goal, max_iteration )
-  end subroutine cg_diagonal_matrix_tl__dp
-
   subroutine cg_symmetric_matrix__dp( n, a, b, x, accuracy_goal, max_iteration )
     integer, intent(in) :: n
     real(kind=dp), intent(in) :: a(n,n)
